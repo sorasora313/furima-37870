b7cae840858a95d458f105d952a283b4821e2d58
sorasora313 <sky.313.sora@gmail.com>
2022-07-17 11:29:06 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,20 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
     
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
 
   private
 
