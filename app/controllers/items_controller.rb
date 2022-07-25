5a162553b70d6d01c83f23c34bbac6bd603952c9
sorasora313 <sky.313.sora@gmail.com>
2022-07-26 08:15:09 +0900

    修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :sign_in, only: [:new]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :order_url, only: [:edit, :update, :destroy]
+
   def index
     @items = Item.all.order("created_at DESC")
  end
@@ -26,14 +26,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    
-    if user_signed_in? && current_user.id == @item.user_id  
+    if @item.user_id == current_user.id && @item.order.nil?
     else
       redirect_to root_path
     end
-
   end
-
   
 
   def update
